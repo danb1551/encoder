3c6a77963024aff84d27ba2f37d04d311df32d2a
danb1551 <157605673+danb1551@users.noreply.github.com>
2024-02-11 20:50:51 +0100

    main.py

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import os
 
 def main():
-    print("Vítejte v aplikaci pro kódování a dekódování.")
-    print("Zvolte možnost:")
-    print("1. Kódování")
-    print("2. Dekódování")
-    choice = input("Zadejte číslo možnosti: ")
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    print("@                              @")
+    print("@Welcome in encoder            @")
+    print("@Created by: Danb1551          @")
+    print("@Instagram: @danik_12sniper    @")
+    print("@Web: termux.wz.cz             @")
+    print("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@")
+    print("Select:")
+    print("{1.} Encode")
+    print("{2.} Dekódování")
+    choice = input("Write down the selected number: ")
 
     if choice == '1':
         os.system('python3 encoder.py')
     elif choice == '2':
         os.system('python3 decoder.py')
     else:
-        print("Neplatná volba.")
+        print("Error")
 
 if __name__ == "__main__":
     main()
